5bffaa4077429a956ec5868b4938ea6b4e461401
kmnxa <108279650+kmnxa@users.noreply.github.com>
2024-11-16 20:49:51 +0100

    Update todo.cy.js
    Better now

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -10,7 +10,6 @@
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
-const cy = {};
 
 describe('example to-do app', () => {
   beforeEach(() => {
